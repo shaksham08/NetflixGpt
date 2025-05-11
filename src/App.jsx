98b00f2f14daccf2b42447208511e179cc528187
shaksham08 <shakshamsinha@outlook.com>
2025-05-11 13:41:01 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-amber-400'> This is netflix gpt</h1>
+    </>
+  )
+}
+
+export default App
